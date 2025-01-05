c5466bcad2b35ca815f34795128d79362715059b
Tajammul Pasha <tajammul@goharbor.xyz>
2025-01-05 09:14:57 +0530

    feat: initialte project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+// import './App.css'
+
+import TestComp from "./TestComp"
+
+function App() {
+  return (
+    <>
+
+      <h1>Hello React locally!</h1>
+    <TestComp />
+    </>
+  )
+}
+
+export default App
---
+++ src/TestComp.jsx
@@ -0,0 +1,5 @@
+const TestComp = () => {
+  return <h1>Test component</h1>
+}
+
+export default TestComp;
